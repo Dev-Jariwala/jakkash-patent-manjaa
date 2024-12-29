6f770d73d0b95027148b0617aa1a11065e8b6b62
Yash Prajapati <yashprajapati2017@gmail.com>
2024-12-29 20:54:26 +0530

    jakkash 0.0.0

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL, 'http://localhost:5173'],
+    origin: [process.env.FRONTEND_URL, 'http://localhost:5173', 'https://jakkash-patent-manjaa.vercel.app'],
     credentials: true,
   })
 );
